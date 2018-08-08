7dff2a31bc837aac6f7ad4cabffa9c90f2b20154
rabiah1 <rabiahn01@gmail.com>
2018-08-08 18:19:05 +0000

    Done.

--- summer_olympics_hash.rb
+++ summer_olympics_hash.rb
@@ -1,7 +1,7 @@
 summer_olympics = {Sydney: "2000", Athens: "2004", Beijing: "2008", London: "2012"}
  
 summer_olympics[:Atlanta] = "1996"
- #puts summer_olympics
+ puts summer_olympics
 
 
 
@@ -13,4 +13,6 @@ puts summer_olympics
 
 
 
-
+summer_olympics.each do |place, year|
+  puts place.upcase
+end
